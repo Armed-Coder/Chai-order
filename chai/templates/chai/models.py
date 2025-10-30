9c8d5e9416101c7ba01b8e51d0356fca40e32689
Megha Choudhary <72974234+Coder-Megha@users.noreply.github.com>
2025-10-30 13:28:14 +0530

    Added models
    Added models.py for chai orders

---
+++ chai/templates/chai/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class chai_variety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('ML' , 'MASALA'),
+        ('GR' , 'GINGER'),
+        ('BT' , 'BLACK'),
+        ('KW' , 'KIWI'),
+        ('EL' , 'ELAICHI'),
+    ]
+    
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    kaisi = models.CharField(max_length=2 , choices = CHAI_TYPE_CHOICE)
+    description = models.TextField(default='')
+    
+    
+    def __str__(self):
+        return self.name
